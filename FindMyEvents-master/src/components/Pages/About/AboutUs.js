a91d9506943870c4538a4519b64aa1d14459040f
Prakhar55 <prakharshrivastava74@gmail.com>
2023-08-27 12:42:51 +0530

    Add files via upload

---
+++ FindMyEvents-master/src/components/Pages/About/AboutUs.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 >About - FindMyEvent</h1>
+            <hr></hr>
+            <h2 class="display-4">App provides details of upcoming events in your region.</h2>
+            <h2 class="display-4">Add and view your events in your profile</h2>
+            <hr></hr>
+            <h6>This app is a product of FindMyMovies. All rights reserverd &copy;</h6>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default AboutUs;
